e93a258f604a08545fd9931a47f370c3b75d23d3
Abner André <abnerndr.dev@gmail.com>
2023-05-28 18:32:58 -0300

    update imagens and colors

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from 'react'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
-import '@/styles/tailwind.css'
+import '@/assets/styles/tailwind.css'
 import 'focus-visible'
 
 function usePrevious(value) {
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,11 +3,11 @@ import Image from 'next/image'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoAnimaginary from '@/assets/images/logos/animaginary.svg'
+import logoCosmos from '@/assets/images/logos/cosmos.svg'
+import logoHelioStream from '@/assets/images/logos/helio-stream.svg'
+import logoOpenShuttle from '@/assets/images/logos/open-shuttle.svg'
+import logoPlanetaria from '@/assets/images/logos/planetaria.svg'
 
 const projects = [
   {
@@ -90,7 +90,7 @@ export default function Projects() {
                 <Card.Link href={project.link.href}>{project.name}</Card.Link>
               </h2>
               <Card.Description>{project.description}</Card.Description>
-              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-violet-500 dark:text-zinc-200">
                 <LinkIcon className="h-6 w-6 flex-none" />
                 <span className="ml-2">{project.link.label}</span>
               </p>
